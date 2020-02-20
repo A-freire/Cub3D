6d6c90a3a6711c51ee72cd0ee960fd518ee7e56f
A-freire <bexas94@gmail.com>
2020-02-20 14:19:15 +0100

    first

---
+++ Makefile
@@ -0,0 +1,40 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: afreire- <afreire-@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2020/02/20 11:06:38 by afreire-          #+#    #+#              #
+#    Updated: 2020/02/20 11:06:52 by afreire-         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+NAME = cub3d
+
+SRCS =	cub3d.c
+
+OBJS = ${SRCS:.c=.o}
+
+CC  = gcc
+
+FLAGS =   -L includes/minilibx -lmlx -framework OpenGL -framework Appkit 
+CFLAGS =  -Wall -Wextra -Werror -I includes/ -I includes/minilibx
+RM  = rm -f
+
+${NAME}	:	${OBJS}
+	@${CC}	 -o ${NAME}  ${FLAGS} ${OBJS}
+
+all:  ${NAME}
+
+clean:	
+	${RM}	${OBJS}
+
+fclean:  clean
+	${RM} ${NAME}
+
+re: fclean all
+	${RM}	${OBJS}
+exe: re
+	./${NAME}
+.PHONY: all clean fclean re
---
+++ cub3d.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afreire- <afreire-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/20 11:02:35 by afreire-          #+#    #+#             */
+/*   Updated: 2020/02/20 14:18:57 by afreire-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./includes"
+void putPixel(unsigned char r, unsigned char g, unsigned char b, char *img_data, int x, int y)
+{
+	img_data[(x + y * 500) * 4] = (char)b;
+	img_data[(x + y * 500) * 4 + 1] = (char)g;
+	img_data[(x + y * 500) * 4 + 2] = (char)r;
+	img_data[(x + y * 500) * 4 + 3] = 0;
+}
+
+int main()
+{
+	void *mlx_ptr;
+	void *win_ptr;
+	void *img_ptr;
+	int osef = 250;
+    char *img_data;
+    int x = 0;
+    int y = 0;
+    
+    mlx_ptr = mlx_init();
+	win_ptr = mlx_new_window(mlx_ptr, x, y, "YOLO");
+	img_ptr = mlx_new_image(mlx_ptr, x, y);
+	img_data = mlx_get_data_addr(img_ptr, &osef, &osef, &osef);
+
+
+    
+    mlx_put_image_to_window(mlx_ptr, win_ptr, img_ptr, 0, 0);
+	mlx_loop(mlx_ptr);
+}
