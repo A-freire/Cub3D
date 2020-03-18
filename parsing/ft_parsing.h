21697cde45be5823bc880ce4d335a8db057371a9
robriard <robriard@student.42.fr>
2020-03-18 10:09:47 +0100

    parsing add

---
+++ parsing/ft_parsing.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_parsing.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: robriard <robriard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/02 15:59:39 by robriard          #+#    #+#             */
+/*   Updated: 2020/03/18 09:34:37 by robriard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PARCING_H
+# define PARCING_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include "./Get-Next-Line/get_next_line.h"
+# include "parsing_struct.h"
+
+int	        ft_parsing(char *file, t_file *s);
+int         *ft_map(char *line);
+int		    **ft_finalmap(int **map);
+t_file      ft_readparsing(char *file, char *line, t_file *s);
+t_res       ft_res(char *line);
+t_color     ft_color(char *line);
+char		*ft_way(char *line);
+
+int		    ft_strncmp(char *s1, char *s2, int x);
+int		    ft_ismap(char *line);
+int		    ft_countmap(char *file);
+void		ft_error(int error);
+
+int		    ft_checkparsing(t_file *s);
+int		    ft_checkmap(int **map, t_spawn spawn);
+
+#endif
