3f3087bff514e672314c6add478a1dccd8fbd1e8
robriard <robriard@student.42.fr>
2020-07-01 16:18:28 +0200

    reset

--- srcs/read.c
+++ srcs/read.c
@@ -6,11 +6,33 @@
 /*   By: afreire- <afreire-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/29 18:34:12 by afreire-          #+#    #+#             */
-/*   Updated: 2020/06/29 18:35:45 by afreire-         ###   ########.fr       */
+/*   Updated: 2020/07/01 16:18:01 by robriard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/cub3d_lib.h"
+
+int		ft_spr(t_all *all, int x, int y, int i)
+{
+	if (all->map.map[x][y] == 2 || all->map.map[x][y] == 3)
+	{
+		all->tex.spritex[i] = y + 0.5;
+		all->tex.spritey[i] = x + 0.5;
+		return (1);
+	}
+	return (0);
+}
+
+int		ft_tlp(t_all *all, int x, int y, int j)
+{
+	if (all->map.map[x][y] == 3 && j < 2)
+	{
+		all->tp.coord[0][j] = y + 0.5;
+		all->tp.coord[1][j] = y + 0.5;
+		return (1);
+	}
+	return (0);
+}
 
 void	trueread(t_all *all)
 {
@@ -28,18 +50,8 @@ void	trueread(t_all *all)
 		x = -1;
 		while (all->map.map[++x][0] != -42)
 		{
-			if (all->map.map[x][y] == 2 || all->map.map[x][y] == 3)
-			{
-				all->tex.spritex[i] = y + 0.5;
-				all->tex.spritey[i] = x + 0.5;
-				i++;
-			}
-			if (all->map.map[x][y] == 3 && j < 2)
-			{
-				all->tp.coord[0][j] = y + 0.5;
-				all->tp.coord[1][j] = x + 0.5;
-				j++;
-			}
+			i += ft_spr(all, x, y, i);
+			j += ft_tlp(all, x, y, j);
 		}
 	}
 }
@@ -59,21 +71,9 @@ void	revread(t_all *all)
 	{
 		y = 0;
 		while (all->map.map[x][y] != -42)
-		{
-			if (all->map.map[x][y] == 2 || all->map.map[x][y] == 3)
-			{
-				all->tex.spritex[i] = y + 0.5;
-				all->tex.spritey[i] = x + 0.5;
-				i++;
-			}
-			if (all->map.map[x][y] == 3 && j < 2)
-			{
-				all->tp.coord[0][j] = y + 0.5;
-				all->tp.coord[1][j] = x + 0.5;
-				j++;
-			}
-			y++;
-		}
-		x++;
+			i += ft_spr(all, x, y, i);
+		j += ft_tlp(all, x, y, j);
+		y++;
 	}
+	x++;
 }
