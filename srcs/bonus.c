364bf060d5d7fde406335f2f56f9a07e438281ed
robriard <robriard@student.42.fr>
2020-07-01 15:12:58 +0200

    norme

--- srcs/bonus.c
+++ srcs/bonus.c
@@ -141,23 +141,23 @@ void	ft_tp(t_all *all)
 		{
 			if (all->map.map[(int)all->tp.coord[1][1] + 1][(int)all->tp.coord[0][1]] == 0)
 			{
-				all->start.pos.x = (double)all->tp.coord[1][1] + 1.5;
+				all->start.pos.x = (double)all->tp.coord[1][1] + 1;
 				all->start.pos.y = (double)all->tp.coord[0][1];
 			}
 			else if (all->map.map[(int)all->tp.coord[1][1] - 1][(int)all->tp.coord[0][1]] == 0)
 			{
-				all->start.pos.x = (double)all->tp.coord[1][1] - 0.5;
+				all->start.pos.x = (double)all->tp.coord[1][1] - 1;
 				all->start.pos.y = (double)all->tp.coord[0][1];
 			}
 			else if (all->map.map[(int)all->tp.coord[1][1]][(int)all->tp.coord[0][1] - 1] == 0)
 			{
 				all->start.pos.x = (double)all->tp.coord[1][1];
-				all->start.pos.y = (double)all->tp.coord[0][1] - 0.5;
+				all->start.pos.y = (double)all->tp.coord[0][1] - 1;
 			}
 			else if (all->map.map[(int)all->tp.coord[1][1]][(int)all->tp.coord[0][1] + 1] == 0)
 			{
 				all->start.pos.x = (double)all->tp.coord[1][1];
-				all->start.pos.y = (double)all->tp.coord[0][1] + 1.5;
+				all->start.pos.y = (double)all->tp.coord[0][1] + 1;
 			}
 			else
 			{
