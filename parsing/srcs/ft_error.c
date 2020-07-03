f867ed00a6fa61e5994bb0838764c4116f58b2fa
robriard <robriard@student.42.fr>
2020-07-03 11:51:55 +0200

    norme

--- parsing/srcs/ft_error.c
+++ parsing/srcs/ft_error.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_error.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: afreire- <afreire-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: robriard <robriard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/27 17:09:00 by robriard          #+#    #+#             */
-/*   Updated: 2020/07/02 10:36:58 by robriard         ###   ########.fr       */
+/*   Updated: 2020/07/03 11:50:48 by robriard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_parsing.h"
 
-static int		ft_printf(char *str)
+static int	ft_printf(char *str)
 {
 	int		i;
 
@@ -25,35 +25,47 @@ static int		ft_printf(char *str)
 	return (i);
 }
 
-void			ft_error(int error)
+void		ft_lessoften(int error)
+{
+	if (error == -1)
+		ft_printf("A pointer has bad allocate");
+	if (error == 1)
+		ft_printf("Bad resolution\n");
+	if (error == 2)
+		ft_printf("The map is open\n");
+	if (error == 3)
+		ft_printf("Unknow way texture\n");
+	if (error == 4)
+		ft_printf("Back Screen incorrect colors\n");
+	if (error == 5)
+		ft_printf("Spawn Uninitialized\n");
+	if (error == 6)
+		ft_printf("A way doesn't exist\n");
+	if (error == 7)
+		ft_printf("A doesn't be in texture folder\n");
+	if (error == 8)
+		ft_printf("You give an acces way for tp texture without tp position\n");
+	if (error == 9)
+		ft_printf("You don't give the good number of tp position\n");
+}
+
+void		ft_moreoften(int error)
+{
+	if (error == 10)
+		ft_printf("You don't give an acces way for tp texture\n");
+	if (error == 42)
+		ft_printf("\033[33:05mYou die!!!!!\nTry Again\n");
+	if (error == 404)
+		ft_printf("No .cub file input\n");
+}
+
+void		ft_error(int error)
 {
 	if (error != 42)
 		ft_printf("\033[31;01mError\033[00m\n");
-	if (error == 404)
-		ft_printf("\033[33;05mNo .cub file input\033[00m\n");
-	if (error == -1)
-		ft_printf("\033[33;05mA pointer has bad allocate");
-	if (error == 1)
-		ft_printf("\033[33;05mBad resolution\033[00m\n");
-	if (error == 2)
-		ft_printf("\033[33;05mThe map is open\033[00m\n");
-	if (error == 3)
-		ft_printf("\033[33;05mUnknow way texture\033[00m\n");
-	if (error == 4)
-		ft_printf("\033[33;05mBack Screen incorrect colors\033[00m\n");
-	if (error == 5)
-		ft_printf("\033[33;05mSpawn Uninitialized\033[00m\n");
-	if (error == 6)
-		ft_printf("\033[33;05mA way doesn't exist\033[00m\n");
-	if (error == 7)
-		ft_printf("\033[33;05mA doesn't be in texture folder\033[00m\n");
-	if (error == 8)
-		ft_printf("\033[33;05mYou give an acces way for tp texture without tp position\033[00m\n");
-	if (error == 9)
-		ft_printf("\033[33;05mYou don't give the good number of tp position\033[00m\n");
-	if (error == 10)
-		ft_printf("\033[33;05mYou don't give an acces way for tp texture\033[00m\n");
-	if (error == 42)
-		ft_printf("\033[33:05mYou die!!!!!\nTry Again\n");
+	if (error < 10)
+		ft_lessoften(error);
+	else
+		ft_moreoften(error);
 	exit(0);
 }
