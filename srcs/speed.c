0a69854d8a6aeb55ee8ba530c734a95d90f5de0d
Adrien FREIRE ELEUTERIO <afreire-@e1r9p10.42.fr>
2020-07-09 16:29:49 +0200

    fix bug fov

--- srcs/speed.c
+++ srcs/speed.c
@@ -6,7 +6,7 @@
 /*   By: afreire- <afreire-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/07 14:55:29 by afreire-          #+#    #+#             */
-/*   Updated: 2020/07/07 14:55:31 by afreire-         ###   ########.fr       */
+/*   Updated: 2020/07/09 16:29:23 by afreire-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,6 @@
 void	normalspeed(t_all *all)
 {
 	all->movespeed = 0.2;
-	if (all->start.fov.x != 0)
-	{
-		if (all->start.fov.x < 0)
-			all->start.fov.x = -0.66;
-		else
-			all->start.fov.x = 0.66;
-	}
-	else
-	{
-		if (all->start.fov.y < 0)
-			all->start.fov.y = -0.66;
-		else
-			all->start.fov.y = 0.66;
-	}
 }
 
 void	sneack(t_all *all)
@@ -36,23 +22,7 @@ void	sneack(t_all *all)
 	if (all->movespeed == 0.3)
 		normalspeed(all);
 	else
-	{
 		all->movespeed = 0.1;
-		if (all->start.fov.x != 0)
-		{
-			if (all->start.fov.x < 0)
-				all->start.fov.x = -0.36;
-			else
-				all->start.fov.x = 0.36;
-		}
-		else
-		{
-			if (all->start.fov.y < 0)
-				all->start.fov.y = -0.36;
-			else
-				all->start.fov.y = 0.36;
-		}
-	}
 }
 
 void	sprint(t_all *all)
@@ -60,21 +30,5 @@ void	sprint(t_all *all)
 	if (all->movespeed == 0.1)
 		normalspeed(all);
 	else
-	{
 		all->movespeed = 0.3;
-		if (all->start.fov.x != 0)
-		{
-			if (all->start.fov.x < 0)
-				all->start.fov.x = -0.96;
-			else
-				all->start.fov.x = 0.96;
-		}
-		else
-		{
-			if (all->start.fov.y < 0)
-				all->start.fov.y = -0.96;
-			else
-				all->start.fov.y = 0.96;
-		}
-	}
 }
